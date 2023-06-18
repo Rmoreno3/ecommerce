92e51a2f34acce15eca53390974a29c0787a00ee
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2023-06-17 22:27:32 -0400

    creando el consumo de la API, refactorizando

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,19 +1,28 @@
+import { useState, useEffect } from "react";
 import Card from "../../Components/Card";
+const API = "https://api.escuelajs.co/api/v1/products";
 
 function Home() {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API);
+        const data = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="flex gap-5 flex-wrap justify-center items-center">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+      {product?.map((product) => (
+        <Card key={product.id} data={product} />
+      ))}
     </div>
   );
 }
